b4e389978142fc9a44fea2a133981b11fe2a02b5
Bhavan Patel <bhavan7@yahoo.com>
2024-09-14 11:31:52 +0530

    Update

--- app/source/techbrew/src/app/app.component.ts
+++ app/source/techbrew/src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'techbrew';
+  lcpImage = '';
+
+  constructor() {
+    this.initLCPAlert();
+  }
+
+  initLCPAlert() {
+    setTimeout(() => {
+      this.lcpImage =
+        'https://fastly.picsum.photos/id/397/1080/1080.jpg?hmac=32tbA6A7Zmn196TksDP73zB4L4RWB2aIi2JyFUsYfpg';
+    }, 10000);
+  }
 }
